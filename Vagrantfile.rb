61bf28db0cecbbad8a93bd619b499ad2287954ae
Marine200326 <150470503+Marine200326@users.noreply.github.com>
2023-12-01 18:53:42 +0200

    Add files via upload

---
+++ Vagrantfile.rb
@@ -0,0 +1,30 @@
+Vagrant.configure("2") do |config| 
+    config.vm.define "docker_host" do |dh|
+    config.vm.box = "ubuntu-linux"
+    config.vm.network "private_network", type: "dhcp", ip: "192.168.56.100"
+    config.vm.network "forwarded_port", guest: 8080, host: 8080, auto_correct: true
+    config.vm.network "forwarded_port", guest: 8081, host: 8081, auto_correct: true
+    config.vm.network "forwarded_port", guest: 8082, host: 8082, auto_correct: true
+    config.vm.network "forwarded_port", guest: 9000, host: 9000, auto_correct: true 
+    dh.vm.provision "shell", path: "docker_bootstrap.sh", privileged: "true"
+  
+    config.vm.provider "virtualbox" do |vb|
+      vb.memory = "1024" 
+      vb.cpus="2"
+    end
+  
+
+  
+#   Vagrant.configure(2) do |config|
+#     config.vm.define "docker_host" do |dh|
+#         dh.vm.box = "debian/bullseye64"
+#         dh.vm.network "private_network", ip: "192.168.56.100"
+#         dh.vm.network :forwarded_port, guest: 8080, host: 8080, auto_correct: true
+#         dh.vm.provision "shell", path: "docker_bootstrap.sh", privileged: "true"
+#         config.vm.provider "virtualbox" do |vb|
+#             vb.memory = "2048"
+#             vb.cpus = "2"
+#             vb.name = "Docker Host"
+#         end
+#     end
+# end
