029d5bcf60de779a51afe6b449e18b7632d739fc
Marine200326 <150470503+Marine200326@users.noreply.github.com>
2023-12-01 19:06:13 +0200

    Update Vagrantfile.rb

--- Vagrantfile.rb
+++ Vagrantfile.rb
@@ -1,6 +1,6 @@
 Vagrant.configure("2") do |config| 
     config.vm.define "docker_host" do |dh|
-    config.vm.box = "ubuntu-linux"
+    config.vm.box = "bento/ubuntu-22.04"
     config.vm.network "private_network", type: "dhcp", ip: "192.168.56.100"
     config.vm.network "forwarded_port", guest: 8080, host: 8080, auto_correct: true
     config.vm.network "forwarded_port", guest: 8081, host: 8081, auto_correct: true
